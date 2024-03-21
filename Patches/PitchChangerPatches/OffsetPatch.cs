f8a132faf8d05c7ba3cfd092f5a335e3350dfc77
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-03-21 12:12:25 -0600

    Renamed and moved the patch

---
+++ Patches/PitchChangerPatches/OffsetPatch.cs
@@ -0,0 +1,22 @@
+using GearBox.Managers;
+using HarmonyLib;
+using Il2CppFormulaBase;
+using UnityEngine;
+
+namespace GearBox.Patches.PitchChangerPatches;
+
+[HarmonyPatch(typeof(StageBattleComponent))]
+internal static class OffsetPatch
+{
+    [HarmonyPatch(nameof(StageBattleComponent.getMusicOffset), MethodType.Getter)]
+    [HarmonyPostfix]
+    internal static void MusicOffset(ref int __result)
+    {
+        if (!SettingsManager.IsEnabled) return;
+
+        if (!ModManager.PitchChangerComp) return;
+        
+        // Add offset
+        __result += AudioSettings.GetConfiguration().dspBufferSize * ModManager.PitchChangerComp.DelayCounter;;
+    }
+}
